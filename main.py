5981a6f2960412326e93c2af9568ce71130eff6f
Khoa Nguyen <115177830+khoawack@users.noreply.github.com>
2022-12-12 01:15:02 -0800

    Update main.py
    removed random codes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pyautogui
 import time
 
+# pip install keyboard | pip install PyAutoGUI
+
 check = (754, 335)
 blue_e = (765, 444)
 
@@ -26,52 +28,3 @@ def sniper():
     # pyautogui.moveTo(blue_e)
 
 name_time()
-
-
-
-
-from account import Account
-
-# desired_name = "lmly"
-#-------------------------------------------------------------- coords checker
-# while True:
-#     riot = pyautogui.position()
-#     time.sleep(1)
-#     print(riot)
-
-# name = (626, 320)
-
-
-
-
-# pyautogui.click(name)
-# pyautogui.typewrite(desired_name)
-
-# time.sleep(3)
-
-
-
-
-
-
-
-
-#login-----------------------------------------------------
-# user1 = Account("khoa")
-#
-# riot = (847, 1064)
-# user_cord = (351, 365)
-# password_cord = (325, 429)
-# enter = (401, 801)
-#
-# pyautogui.click(riot)
-# time.sleep(8)
-# pyautogui.click(user_cord)
-# time.sleep(.4)
-# user1.user()
-#
-# pyautogui.click(password_cord)
-# user1.password()
-#
-# pyautogui.click(enter)
-
